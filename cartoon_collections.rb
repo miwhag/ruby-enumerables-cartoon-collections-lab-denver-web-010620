64c28748a7a7d0b21dc1407a99a8a3d1997920d3
Miwha Geschwind <miwhag@gmail.com>
2020-01-01 23:10:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,13 +33,11 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  # if array.include?(cheese_types[0] ) 
   array.map do |cheese|
     if cheese == cheese_types[0] || cheese == cheese_types[1] || cheese == cheese_types[2]
       return cheese
     end
   end
-  # end 
 return nil 
 end
 
